d217a76e9274804a53687d1ba3d4c87b653a4b80
Manuel Guenther <manuel.guenther@idiap.ch>
2015-05-20 14:53:37 +0200

    Based get_config on bob.extension

--- bob/learn/boosting/__init__.py
+++ bob/learn/boosting/__init__.py
@@ -5,6 +5,11 @@ import bob.io.base
 import bob.extension
 bob.extension.load_bob_library('bob.learn.boosting', __file__)
 
+# versioning
+from . import version
+from .version import module as __version__
+from .version import api as __api_version__
+
 # include loss functions
 from . import LossFunction # Just to get the documentation for it
 from .ExponentialLoss import ExponentialLoss
@@ -26,23 +31,8 @@ from ._library import weighted_histogram
 def get_config():
   """Returns a string containing the configuration information.
   """
+  return bob.extension.get_config(__name__, version.externals, version.api)
 
-  import pkg_resources
-  from .version import externals
-
-  packages = pkg_resources.require(__name__)
-  this = packages[0]
-  deps = packages[1:]
-
-  retval =  "%s: %s [api=0x%04x] (%s)\n" % (this.key, this.version,
-      version.api, this.location)
-  retval += "  - c/c++ dependencies:\n"
-  for k in sorted(externals): retval += "    - %s: %s\n" % (k, externals[k])
-  retval += "  - python dependencies:\n"
-  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
-
-  return retval.strip()
 
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
-
